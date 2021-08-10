8a7050817ab3cfb9b5bfcb01314f9447b6f9acc7
LalitLalit12 <88704171+LalitLalit12@users.noreply.github.com>
2021-08-10 12:14:56 +0530

    Add files via upload

---
+++ App/app.js
@@ -0,0 +1,11 @@
+var msg= ""
+if (age>=18)
+{
+    msg="You are eli8gible for vote!?"
+
+}
+else{
+    msg="You are not eligible for vote, But you  are eligible after+(18-age)+year";
+}
+document.getElementById("msg").innerHtml=msg;
+document.getElementById("btn").addeventlistener ("click").validate;
